1ca4cc7ef8b50f053eb94cf64cc92d18da235c6b
ran <rmalka@trigoretail.com>
2024-10-06 14:24:00 +0300

    fixed the first print

--- world_of_games/code/app.py
+++ world_of_games/code/app.py
@@ -18,11 +18,11 @@ def welcome():
     print("Hi " + username + " and welcome to the World of Games:")
     print("*" * 15)
 def start_play():
-    print("Here the list of Games")
-    print("1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back. ")
-    print("2. Guess Game - guess a number and see if you chose like the computer.")
-    print("3. Currency Roulette - try and guess the value of a random amount of USD in ILS")
-    print("4. Quit")
+    print("""Here the list of Games")
+    1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back.
+    2. Guess Game - guess a number and see if you chose like the computer.
+    3. Currency Roulette - try and guess the value of a random amount of USD in ILS
+    4. Quit""")
     game_choice = input("Please choose a Game:")
 
     if int(game_choice) < 1 or int(game_choice) > 4:
